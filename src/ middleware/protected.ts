58ba363efcdee7de88d1790842031b678addee46
wisnucakraa <wisnucakraa@gmail.com>
2023-07-16 03:29:00 +0700

    change cookies with bearer auth

--- src/ middleware/protected.ts
+++ src/ middleware/protected.ts
@@ -1,38 +1,53 @@
 import { RequestHandler } from "express";
 import ErrorHandling from "../util/errorHandling";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import User from "../model/user";
 
-
 const verifiedUser: RequestHandler = async (req, res, next) => {
-    const cookies = req.headers.cookie;
-    if(!cookies) return ErrorHandling({
-        statusCode: 400,
-        message: "No Cookies Provided",
-        res: res
-    });
-    const at = cookies.split('=')[1];
-    if(!at) return ErrorHandling({
-        statusCode: 400,
-        message: "No Access Token",
-        res: res
-    });
+  const authHeader = req.headers.authorization;
 
-    const decoded = jwt.verify(at, process.env.SECRET_HASH as string) as {id: string};
-    if(!decoded.id) return ErrorHandling({
-        statusCode: 403,
-        message: "Invalid Token",
-        res: res
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return ErrorHandling({
+      statusCode: 401,
+      message: "Invalid or missing bearer token",
+      res: res,
     });
+  }
+
+  const token = authHeader.substring(7);
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_HASH as string) as {
+      id: string;
+    };
+
+    if (!decoded.id) {
+      return ErrorHandling({
+        statusCode: 403,
+        message: "Invalid token",
+        res: res,
+      });
+    }
 
     const validUser = await User.findById(decoded.id);
-    if(!validUser) return ErrorHandling({
+
+    if (!validUser) {
+      return ErrorHandling({
         statusCode: 403,
-        message: "Invalid Token",
-        res: res
-    });
-    req.user = validUser
+        message: "Invalid token",
+        res: res,
+      });
+    }
+
+    req.user = validUser;
     next();
+  } catch (error) {
+    return ErrorHandling({
+      statusCode: 401,
+      message: "Invalid token",
+      res: res,
+    });
+  }
 };
 
 export default verifiedUser;
--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -2,49 +2,59 @@ import { RequestHandler } from "express";
 import ErrorHandling from "../util/errorHandling";
 import User from "../model/user";
 import { createHmac } from "crypto";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 interface ILoginInput {
-    username: string;
-    password: string;
-};
+  username: string;
+  password: string;
+}
 export const loggingIn: RequestHandler = async (req, res, next) => {
-    if(!req.body) return ErrorHandling({
-            statusCode: 400,
-            message: "Missing Request Body",
-            res: res
-        });
-
-    const userInput = req.body as ILoginInput;
-    if(!userInput.username || !userInput.password) return ErrorHandling({
-            statusCode: 400,
-            message: "Missing Required Field",
-            res: res
-        });
-
-    const foundedUser = await User.findOne({username: userInput.username});
-    if(!foundedUser) return ErrorHandling({
-        statusCode: 404,
-        message: "Username not found",
-        res: res
+  if (!req.body)
+    return ErrorHandling({
+      statusCode: 400,
+      message: "Missing Request Body",
+      res: res,
     });
 
-    const hashedInput = createHmac('sha256', process.env.SECRET_HASH as string).update(userInput.password).digest('hex');
-    const passwordValid = foundedUser.password === hashedInput;
-    if(!passwordValid) return ErrorHandling({
-        statusCode: 403,
-        message: "Password salah",
-        res: res
+  const userInput = req.body as ILoginInput;
+  console.log(userInput);
+  if (!userInput.username || !userInput.password)
+    return ErrorHandling({
+      statusCode: 400,
+      message: "Missing Required Field",
+      res: res,
     });
 
-    const at = jwt.sign({id: foundedUser._id, role: foundedUser.role, name: foundedUser.bio.name}, process.env.SECRET_HASH as string);
-    
+  const foundedUser = await User.findOne({ username: userInput.username });
+  if (!foundedUser)
+    return ErrorHandling({
+      statusCode: 404,
+      message: "Username not found",
+      res: res,
+    });
 
-    res.setHeader('Set-Cookie', `jwt_at=${at};HttpOnly;SameSite=None;Path=/;Secure`);
+  const hashedInput = createHmac("sha256", process.env.SECRET_HASH as string)
+    .update(userInput.password)
+    .digest("hex");
 
-    return res.status(200).json({
-        access_token: at
-    })
+  const passwordValid = foundedUser.password === hashedInput;
+  if (!passwordValid)
+    return ErrorHandling({
+      statusCode: 403,
+      message: "Password salah",
+      res: res,
+    });
+
+  const at = jwt.sign(
+    { id: foundedUser._id, role: foundedUser.role, name: foundedUser.bio.name },
+    process.env.SECRET_HASH as string
+  );
+
+  res.setHeader("Authorization", `Bearer ${at}`);
+
+  return res.status(200).json({
+    access_token: at,
+  });
 };
 
 export default loggingIn;
