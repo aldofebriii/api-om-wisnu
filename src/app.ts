334838b36dba0454047f686c4fd18b3bbe8e7d1e
wisnucakraa <wisnucakraa@gmail.com>
2023-07-16 03:29:13 +0700

    remove cors object

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,59 @@
-import express, { Express } from 'express';
-import mongoose from 'mongoose';
-import dotaenv from 'dotenv';
-import { createNewUser, generateSurat, getSurat, getUser, deleteSurat } from './controller/main';
-import loggingIn from './controller/auth';
-import fs from 'fs';
-import path from 'path';
-import https from 'https';
-import verifiedUser from './ middleware/protected';
-import cors from 'cors';
-import helmet from 'helmet';
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import dotaenv from "dotenv";
+import {
+  createNewUser,
+  generateSurat,
+  getSurat,
+  getUser,
+  deleteSurat,
+} from "./controller/main";
+import loggingIn from "./controller/auth";
+import fs from "fs";
+import path from "path";
+import https from "https";
+import verifiedUser from "./ middleware/protected";
+import cors from "cors";
+import helmet from "helmet";
 
 dotaenv.config();
 
-const key = fs.readFileSync(path.join(__dirname, '..', 'selfsigned.key'));
-const cert = fs.readFileSync(path.join(__dirname, '..', 'selfsigned.crt'));
+const key = fs.readFileSync(path.join(__dirname, "..", "selfsigned.key"));
+const cert = fs.readFileSync(path.join(__dirname, "..", "selfsigned.crt"));
 const app: Express = express();
 
-const server = https.createServer({
+const server = https.createServer(
+  {
     key: key,
-    cert: cert
-}, app);
+    cert: cert,
+  },
+  app
+);
 
-app.use(cors({
-    credentials: true,
-    origin: ['https://localhost:3000'],
-    allowedHeaders: ['Content-Type', 'Set-Cookie', 'Authorization', 'Accept'],
-    methods: ['POST', 'GET', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
-}));
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 //Surat
-app.get('/surat/:suratId', verifiedUser, getSurat);
-app.get('/surat', verifiedUser, getSurat);
-app.delete('/surat/:suratId', verifiedUser, deleteSurat);
-app.put('/surat/:suratId', verifiedUser, generateSurat);
-app.post('/surat', verifiedUser, generateSurat);
+app.get("/surat/:suratId", verifiedUser, getSurat);
+app.get("/surat", verifiedUser, getSurat);
+app.delete("/surat/:suratId", verifiedUser, deleteSurat);
+app.put("/surat/:suratId", verifiedUser, generateSurat);
+app.post("/surat", verifiedUser, generateSurat);
 //User
-app.get('/user', verifiedUser ,getUser);
-app.post('/user', createNewUser);
+app.get("/user", verifiedUser, getUser);
+app.post("/user", createNewUser);
 //Auth
-app.post('/login', loggingIn);
+app.post("/login", loggingIn);
 //Util
-app.get('/', (_, res, __) => {
-    return res.status(200).json({
-        msg: "Server is Alived"
-    });
-})
+app.get("/", (_, res, __) => {
+  return res.status(200).json({
+    msg: "Server is Alived",
+  });
+});
 
-mongoose.connect(process.env.MongoURI as string)
-.then(()=> {
+mongoose
+  .connect(process.env.MongoURI as string)
+  .then(() => {
     server.listen(process.env.PORT, () => console.log("Server is Connected"));
-})
-.catch(err => console.log(err));    
+  })
+  .catch((err) => console.log(err));
