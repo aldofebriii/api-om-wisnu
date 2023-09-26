546e4692c4b9a4e896bbeab372cc895824f78e8f
Aldo Junaidi Pebry <64087037+aldofebriii@users.noreply.github.com>
2023-09-26 23:09:16 +0700

    lalala
    ubah sikit2 y g nuk

--- src/controller/admin.ts
+++ src/controller/admin.ts
@@ -8,43 +8,49 @@ import Pengantar from "../model/surat/pengantar";
 import Renovasi from "../model/surat/renovasi";
 
 export const adminGetAllUser: RequestHandler = async (req, res, next) => {
-  const reqAdmin = req.admin;
-  // if (req.body)
-  //   return ErrorHandling({
-  //     statusCode: 403,
-  //     message: "Invalid Cookie",
-  //     res: res,
-  //   });
-
-  const users = await User.find();
-
-  return res.status(200).json(users);
+    try {
+        const reqAdmin = req.admin;
+        if (!reqAdmin)
+          throw ErrorHandling({
+            statusCode: 403,
+            message: "Invalid Cookie"
+          });
+      
+        const users = await User.find();
+      
+        return res.status(200).json(users);
+    } catch(err){
+        next(err);
+    };
 };
 
 export const adminGetAllSurat: RequestHandler = async (req, res, next) => {
-  const reqAdmin = req.admin;
-  // if (req.body)
-  //   return ErrorHandling({
-  //     statusCode: 403,
-  //     message: "Invalid Cookie",
-  //     res: res,
-  //   });
+    try {
+        const reqAdmin = req.admin;
+        if (!reqAdmin)
+            throw ErrorHandling({
+            statusCode: 403,
+            message: "Invalid Cookie"
+            });
 
-  const result = [];
-  const acaras = await Acara.find({});
-  result.push(...acaras);
+        const result = [];
+        const acaras = await Acara.find({});
+        result.push(...acaras);
 
-  const domisili = await Domisili.find({});
-  result.push(...domisili);
+        const domisili = await Domisili.find({});
+        result.push(...domisili);
 
-  const kelahiran = await Kelahiran.find({});
-  result.push(...kelahiran);
+        const kelahiran = await Kelahiran.find({});
+        result.push(...kelahiran);
 
-  const pengantar = await Pengantar.find({});
-  result.push(...pengantar);
+        const pengantar = await Pengantar.find({});
+        result.push(...pengantar);
 
-  const renovasi = await Renovasi.find({});
-  result.push(...renovasi);
+        const renovasi = await Renovasi.find({});
+        result.push(...renovasi);
 
-  return res.status(200).json(result);
+        return res.status(200).json(result);
+    } catch(err){
+        next(err);
+    };
 };
